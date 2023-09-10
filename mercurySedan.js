5b1b9cc36b4a5d0bbb2fe55a027910fc8a2e873b
Arianna Mireles <arianna.mireles02@gmail.com>
2023-09-09 22:35:21 -0500

    Insert methods

--- mercurySedan.js
+++ mercurySedan.js
@@ -12,17 +12,44 @@ console.log(v.make)
 
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
-
-
-
-
-
-
-
-
-
-
-
+class Car extends Vehicle {
+    constructor(make, model, year, color, mileage, maximumPassengers, passengers, numberOfWheels, maximumSpeed, fuel, scheduleService) {
+        super(make, model, year, color, mileage);
+        this.maximumPassengers = maximumPassengers;
+        this.passengers = passengers;
+        this.numberOfWheels = numberOfWheels;
+        this.maximumSpeed = maximumSpeed;
+        this.fuel = fuel;
+        this.scheduleService = scheduleService;
+    }
+    loadPassenger(num) {
+        if (this.passengers < this.maximumPassengers) {
+            if ((num + this.passenger) <= this.maximumPassengers) {
+                this.passenger = num;
+                return this.passenger;
+            } else {
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+    start() {
+        if (this.fuel > 0) {
+            console.log('Engine has started.');
+            return this.started = true;
+        } else {
+            console.log('No fuel.');
+            return this.started = false;
+        }
+    }
+    scheduleService() {
+        if (this.mileage > 30000) {
+            this.scheduleService = true;
+            return this.scheduleService;
+        }
+    }
+}
 
 //TO DO: Creating Instances and Testing Them
 
